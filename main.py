72b1c0b39ba02690b13d051051e22d8735cb95c1
Nasir2023 <129264930+Nasir2023@users.noreply.github.com>
2023-05-12 08:32:22 +0200

    Strings
    With new corrections.

--- main.py
+++ main.py
@@ -12,20 +12,18 @@ print(scorers)
 report = f'{player1} scored in the {goal_0}nd minute\n{player2} scored in the {goal_1}th minute'
 print(report)
 #part 2
-player= "Ruud Gullit"
-spatie= player.find(' ')
-first_name= player[:spatie]
-last_name= player[spatie:]
+player= 'Ruud Gullit'
+#spatie= player.find(' ')
+first_name= player[ :4]
+last_name= player[5:]
+print(last_name)
 last_name_len= len(last_name)
-name_short=player [0:1] +'.' + last_name
+name_short=player [0:1] + '. ' + last_name
 print(name_short)
 print(first_name)
-chant= (first_name + '!')* len(first_name)
-chantzonderspatie= chant[0:-1]
+chant1=(first_name +'! ')* len(first_name)
+chant= chant1[0:-1]
 good_chant=(chant !=' ')
 print(chant)
-print(chantzonderspatie)
-print(last_name)
-print(name_short)
 print(good_chant)
 print(last_name_len)
