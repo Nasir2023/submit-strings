d47a892c3d5d1fbb9742706c72590957d8bfb433
Nasir2023 <129264930+Nasir2023@users.noreply.github.com>
2023-06-30 10:13:54 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,49 @@
 # Do not modify these lines
-__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
-__human_name__ = 'strings'
+__winc_id__ = '7b9401ad7f544be2a23321292dd61cb6'
+__human_name__ = 'arguments'
 
 # Add your code after this line
-player1='Ruud Gullit'
-player2='Marco van Basten'
-goal_0= 32
-goal_1= 54
-scorers= player1 + ' ' + str(goal_0) +', '  + player2 +' '+ str( goal_1)
-print(scorers)
-report = f'{player1} scored in the {goal_0}nd minute\n{player2} scored in the {goal_1}th minute'
-print(report)
-#part 2
-player= 'Ruud Gullit'
-spatie= player.find(' ')
-first_name= player[:spatie]
-last_name= player[spatie + 1:]
-print(last_name)
-last_name_len= len(last_name)
-name_short=player [0:1] + '. ' + last_name
-print(name_short)
-print(first_name)
-chant1=(first_name +'! ')* len(first_name)
-chant= chant1[0:-1]
-good_chant=(chant !=' ')
-print(chant)
-print(good_chant)
-print(last_name_len)
+def greet(name, greeting='Hello, <name>!'):
+   return greeting.replace('<name>', name)
+
+
+greet('Bob')
+greet('Bob', "What's up, <name>!")
+
+
+
+#Part 2: Force
+
+planets= {'mercury': 3.7,
+             'sun': 274,
+             'jupiter': 23.1,
+             'earth' : 9.8,
+             'neptune': 11.0,
+             'saturn': 9,
+             'uranus': 8.9,
+             'venus': 8.9,
+             'mars': 3.7,
+             'moon': 1.6,
+             'pluto': 0.7,
+             }
+def force(mass, body='earth'):
+      for planet, gravity in planets.items():
+          if planet== body:
+              planet_force= mass * gravity
+              print(planet_force)
+              return planet_force
+     
+   
+
+force(5.97*10**24)
+force(1898*10**24, body= 'jupiter')
+
+#Part 3: Gravity
+g=6.674*10**-11
+def pull(m1,m2,d):
+     pull=g*((m1*m2)/d**2)
+     return(pull)
+
+
+print(pull(800, 1500, 3))
+print(pull(0.1, 9972*10**24, 6.371*10**6))
